320a1e59069aebd28b28bf54758b7c3b8c712d11
Prashant Kumar <prashantbittu123@gmail.com>
2021-08-16 18:38:39 +0530

    selection sort

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void selection_sort(int arr[],int n)
+{
+    int min;
+    for(int i=0;i<n-1;i++)
+    {
+        min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[min])
+            {
+                min=j;
+            }
+        }
+        swap(arr[i],arr[min]);
+    }
+}
+
+void swap(int &a,int &b)
+{
+    int c;
+    c=a;
+    a=b;
+    b=c;
+}
+
+void show(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter size of array=";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter element at index "<<i<<" = ";
+        cin>>arr[i];
+    }
+    cout<<"Array: ";
+    show(arr,n);
+    selection_sort(arr,n);
+    cout<<"Sorted Array: ";
+    show(arr,n);
+}
