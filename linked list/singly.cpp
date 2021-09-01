dd1ae548df55f87a34eeef287424d4cca4c2d7a8
Prashant Kumar <prashantbittu123@gmail.com>
2021-09-01 17:56:18 +0530

    singly linked list skeleton

---
+++ linked list/singly.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int option;
+    while(1)
+    {
+        cout<<"Singly linked list"<<endl;
+        cout<<"1.create a list"<<endl;
+        cout<<"2.display"<<endl;
+        cout<<"3.count"<<endl;
+        cout<<"4.search"<<endl;
+        cout<<"5.add to empty list / add at beginning"<<endl;
+        cout<<"6.add at end"<<endl;
+        cout<<"7.add after node"<<endl;
+        cout<<"8.add before node"<<endl;
+        cout<<"9.add at position"<<endl;
+        cout<<"10.delete"<<endl;
+        cout<<"11.reverse"<<endl;
+        cout<<"12.quit"<<endl;
+        cout<<"Select the desired option:";
+        cin>>option;
+        switch(option)
+        {
+            case 1:
+                break;
+            
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+            
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+            
+            case 8:
+                break;
+            
+            case 9:
+                break;
+            
+            case 10:
+                break;
+            
+            case 11:
+                break;
+            
+            case 12:
+                exit(1);
+            
+            default:
+                cout<<"invalid option"<<endl;
+        }
+    }
+    return 0;
+}
